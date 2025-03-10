4a8e904b6c286ce50a9069403f796e149199f187
DANNYWW001 <lekandaniel005@gmail.com>
2025-03-10 06:52:30 +0100

    updated

---
+++ variables.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+let x = 8;
+let y = 5;
+console.log(x, y);
+const a = 10;
+console.log(a)
+const b = 0;
+const B = 90;
+const w = 67;
+const weight = 67;
+const accountNumber = 82872778;
+const myAge = 78;
