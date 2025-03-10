d42aeff3df3be4de266da58d82de871799398a05
DANNYWW001 <lekandaniel005@gmail.com>
2025-03-10 07:14:11 +0100

    uptodate

---
+++ app..js
@@ -0,0 +1,11 @@
+const firstName = 'john';
+const lastName = 'Doe';
+
+
+console.log(firstName.length);
+console.log(lastName.length);
+
+const myPassword = "dannywwave";
+console.log(myPassword);
+console.log(myPassword.length);
+
